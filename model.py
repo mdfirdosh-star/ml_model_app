c20ab70426d9821922581ddacf11a0492216d724
MD FIRDOSH ALAM <mdfirdoshalam851202@gmail.com>
2025-10-28 18:28:10 +0530

    Update model.py

--- model.py
+++ model.py
@@ -22,6 +22,20 @@ def health_check():
     return {"status":"ok",
             "vesion":MODEL_VERSION,
             "model_load":model is not None}
+@app.get("/predict",response_model=predictionresponce)
+def predict_p(data:UserInput):
+    user_input={
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'life_risk': data.life_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation}
+    try:
+         prediction=predict_output(user_input)
+         return JSONResponse(status_code=200,content={"Responce":prediction})
+    except Exception as e:
+        return JSONResponse(status_code=500,content=str(e))
 @app.post("/predict",response_model=predictionresponce)
 def predict_primium(data:UserInput):
     user_input={
